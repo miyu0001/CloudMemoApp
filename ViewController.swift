88bca577ea36fc38f7dbca871ba0711504671d3e
Miyu Sato <miyuba0192@gmail.com>
2021-07-18 22:25:19 +0900

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudMemoApp
+//
+//  Created by 佐藤未悠 on 2021/07/18.
+//
+
+import UIKit
+//練習
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
