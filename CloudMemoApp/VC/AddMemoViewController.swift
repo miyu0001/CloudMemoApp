848e095933e782a0fb534ef7d5a6f5e9fd0bdafb
Miyu Sato <miyuba0192@gmail.com>
2021-07-19 12:43:16 +0900

    storybordの作成

--- CloudMemoApp/VC/AddMemoViewController.swift
+++ CloudMemoApp/VC/AddMemoViewController.swift
@@ -6,16 +6,34 @@
 //
 
 import UIKit
+import NCMB
 
 class AddMemoViewController: UIViewController {
 
     @IBOutlet weak var memoTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
     @IBAction func save(_ sender: Any) {
-        
+        let object = NCMBObject(className: "Memo")
+        object?.setObject(memoTextView.text, forKey: "memo")
+        object?.saveInBackground({ error in
+            if error != nil {
+                //保存失敗
+                print(error)
+            } else {
+                //保存成功したらアラート表示
+                let alertController = UIAlertController(title: "保存完了", message: "メモの保存が完了しました。", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default) { action in
+                    //画面一個前に戻る
+                    self.navigationController?.popViewController(animated: true)
+                }
+                alertController.addAction(action)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        })
     }
 }
